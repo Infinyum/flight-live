71a26bc5db06c7fbb3d87ae967795cf85b0ba428
Robin <Infinyum@users.noreply.github.com>
2018-06-01 14:39:10 +0200

    Parser + classes improvements

--- src/model/CSVParser.java
+++ src/model/CSVParser.java
@@ -14,11 +14,8 @@ final class CSVParser {
      */
     static ArrayList<Country> parseCSV() {
 
-        ArrayList<Airport> airports = new ArrayList<>();
+        ArrayList<City> cities = new ArrayList<>();
         ArrayList<Country> countries = new ArrayList<>();
-
-        // Temporary variables
-        Airport currentAirport;
 
         try {
             FileReader file = new FileReader("deps/airports.csv");
@@ -29,14 +26,29 @@ final class CSVParser {
                 String[] array = line.split(",");
 
                 String name = array[0];
-                String city = array[1];
-                String country = array[2];
+                String cityName = array[1];
+                String countryName = array[2];
                 String icao = array[3];
                 double lat = Double.parseDouble(array[4]);
                 double lon = Double.parseDouble(array[5]);
 
-                currentAirport = new Airport(name, city, country, icao, lat, lon);
-                airports.add(currentAirport);
+                Country country = new Country(countryName);
+                if (countries.contains(country)) {
+                    country = getCountryByName(countries, countryName);
+                }
+                else {
+                    countries.add(country);
+                }
+
+                City city = new City(cityName, country);
+                if (cities.contains(city))
+                    city = getCityByNameAndCountry(cities, cityName, country);
+                else
+                    cities.add(city);
+
+                Airport airport = new Airport(name, city, icao, lat, lon);
+                city.addAirport(airport);
+                country.addCity(city);
 
                 line = bufRead.readLine();
             }
@@ -49,5 +61,34 @@ final class CSVParser {
         }
 
         return countries;
+    }
+
+
+    /**
+     * Look for a country with a specified name in a list
+     * @param countries: the list of countries
+     * @param countryName: the name of the country we are looking for
+     * @return the country object if it was found, null otherwise
+     */
+    private static Country getCountryByName(ArrayList<Country> countries, String countryName) {
+        for (Country c : countries) {
+            if (c.getName().equals(countryName)) return c;
+        }
+        return null;
+    }
+
+
+    /**
+     * Look for a city with a specified name and country in a list
+     * @param cities: the list of cities
+     * @param cityName: the name of the city we are looking for
+     * @param country: the country in which the city is supposed to be
+     * @return the city object if it was found, null otherwise
+     */
+    private static City getCityByNameAndCountry(ArrayList<City> cities, String cityName, Country country) {
+        for (City c : cities) {
+            if (c.getCountry().equals(country) && c.getName().equals(cityName)) return c;
+        }
+        return null;
     }
 }
--- src/model/City.java
+++ src/model/City.java
@@ -4,15 +4,16 @@ import java.util.ArrayList;
 
 public final class City {
     private String name;
+    private Country country;
     private ArrayList<Airport> airports;
 
     /**
      * Constructor for a City object
      * @param n: name of the city
-     * @param a: array of the airports in the city
+     * @param c: the country in which this city is located
      */
-    public City(String n, ArrayList<Airport> a) {
-        name = n; airports = a;
+    public City(String n, Country c) {
+        name = n; country = c; airports = null;
     }
 
 
@@ -21,4 +22,44 @@ public final class City {
      * @return the name of this city
      */
     public String getName() { return name; }
+
+
+    /**
+     * Getter for the country attribute
+     * @return the country in which this city is located
+     */
+    public Country getCountry() { return country; }
+
+
+    /**
+     * Getter for the airports attribute
+     * @return the list of airports in this city
+     */
+    public ArrayList<Airport> getAirports() { return airports; }
+
+
+    /**
+     * Override of the equals method
+     * It only compares the name of the specified country on purpose
+     * @param o: the object to compare
+     * @return true if these objects are considered equal, false otherwise
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        City city = (City) o;
+        return getName().equals(city.getName()) &&
+                getCountry().equals(city.getCountry());
+    }
+
+
+    /**
+     * Add an airport to the airport array
+     * @param a: the airport to add
+     */
+    public void addAirport(Airport a) {
+        if (a != null)
+            getAirports().add(a);
+    }
 }
